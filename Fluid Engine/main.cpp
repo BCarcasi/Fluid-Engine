10630c92832780be31207cad8e2a33c7599dfca1
Ben Carcasi <ben.carcasi@gmail.com>
2022-11-14 10:17:50 -0600

    Newest Working Transluscent

--- Fluid Engine/main.cpp
+++ Fluid Engine/main.cpp
@@ -24,6 +24,9 @@ float angleY = 0.0;
 float lx = 0.0f, lz = -1.0f, ly = 0.0f, dis = 10.0f;
 // XZ position of the camera
 float camX = 0.0f, camY = 1.0f, z = 5.0f;
+bool freeze = false;
+int numOfFrames = 100;
+
 
 int numberOfPoints = 0, frame = 0, count = 0, framesBetweenSwitch = 4;
 
@@ -51,6 +54,22 @@ struct Point
 };
 std::vector< Point > points;
 std::vector<std::vector<Point>> arrayOfPoints;
+
+bool basicCompareOpenGLPoint(Point p1, Point p2) {
+
+	return p1.y < p2.y;
+}
+
+bool trueCompareOpenGLPoint(Point p1, Point p2) {
+
+	return std::pow(p1.x - (camX - lx * dis), 2) + std::pow(p1.y - (camY - ly * dis), 2) + std::pow(p1.z - (z - lz * dis), 2)
+	< std::pow(p2.x - (camX - lx * dis), 2) + std::pow(p2.y -( camY - ly * dis), 2) + std::pow(p2.z - (z - lz * dis), 2);
+}
+
+bool testTrueCompareOpenGLPoint(Point p1, Point p2) {
+	return ((p1.x - (camX - lx * dis)) * sin(angle) * cos(angleY) + (p1.y - (camY - ly * dis)) * -sin(angleY) + (p1.z - (z - lz * dis)) * cos(angle) * cos(angleY))
+	> ((p2.x - (camX - lx * dis)) * sin(angle) * cos(angleY) + (p2.y - (camY - ly * dis)) * -sin(angleY) + (p2.z - (z - lz * dis)) * cos(angle) * cos(angleY));
+}
 
 
 
@@ -104,10 +123,29 @@ void processNormalKeys(unsigned char key, int x, int y) {
 	}
 	if (key == 'z') camY += 0.2f;
 	if (key == 'x') camY -= 0.2f;
-	if (key == 'k') framesBetweenSwitch -= 1;
+	if (key == 'k' && framesBetweenSwitch > 1) framesBetweenSwitch -= 1;
 	if (key == 'j') framesBetweenSwitch += 1;
+	if (key == 'n') {
+		for (int i = 0; i < arrayOfPoints.size(); i++) {
+			points = arrayOfPoints.at(i);
+			std::sort(points.begin(), points.end(), basicCompareOpenGLPoint);
+			std::cout << i << std::endl;
+		}
+	}
+	if (key == 'b') {
+		for (int i = 0; i < arrayOfPoints.size(); i++) {
+			points = arrayOfPoints.at(i);
+			std::sort(points.begin(), points.end(), trueCompareOpenGLPoint);
+			std::cout << i << std::endl;
+		}
+	}
+	if (key == 'm') std::cout << camX - lx * dis << " " << camY - ly * dis << camX - lx * dis << " " << z - lz * dis << " "
+		<< cos(angle) << " " << sin(angle) << " " << sin(angleY) << std::endl;
+	if (key == 'i') freeze = (1 - freeze);
 		
 }
+
+
 
 void renderScene(void) {
 
@@ -133,14 +171,14 @@ void renderScene(void) {
 
 
 	points = arrayOfPoints.at(frame);
-
+	
 
 	glColor3ub(255, 255, 255);
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
 	glVertexPointer(3, GL_FLOAT, sizeof(Point), &points[0].x);
 	glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Point), &points[0].r);
-	glPointSize(2.0);
+	glPointSize(2.0 + pow(3.0f/(dis+0.8),2));
 	glDrawArrays(GL_POINTS, 0, points.size());
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_COLOR_ARRAY);
@@ -148,8 +186,8 @@ void renderScene(void) {
 	glFlush();
 
 	glutSwapBuffers();
-	if (count == 3){
-		frame = (frame + 1) % 100;
+	if (count == 0 && !freeze){
+		frame = (frame + 1) % numOfFrames;
 	}
 	count = (count + 1) % framesBetweenSwitch;
 }
@@ -194,6 +232,8 @@ void processSpecialKeys(int key, int xx, int yy) {
 	}
 }
 
+
+
 int main(int argc, char** argv) {
 
 	// init GLUT and create window
@@ -201,7 +241,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
+	glutInitWindowSize(640, 640);
 	glutCreateWindow("RealTimeRenderingTest");
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_BLEND);
@@ -220,11 +260,21 @@ int main(int argc, char** argv) {
 
 	std::string outputDir = APP_NAME "_output3";
 	jet::Coordinates coordinates;
-	for (int i = 0; i < 100; i++) {
+	std::vector<bool> redBlue;
+	coordinates = loadXYZFromFile(outputDir, 0);
+	for (size_t i = 0; i < coordinates.values.size(); i++) {
+		redBlue.push_back(coordinates.values.at(i).x>1.0);
+	}
+
+	for (int i = 0; i < numOfFrames; i++) {
 		coordinates = loadXYZFromFile(outputDir, i);
 		numberOfPoints = coordinates.values.size();
 		std::cout << i << std::endl;
 		points.clear();
+		
+
+
+
 		for (size_t i = 0; i < coordinates.values.size(); i++)
 		{
 			Point pt;
@@ -234,9 +284,11 @@ int main(int argc, char** argv) {
 			pt.r = 1;
 			pt.g = 1;
 			pt.b = 255;
-			pt.a = 50;
+			pt.a = 8;
+
 			points.push_back(pt);
 		}
+		sort(points.begin(), points.end(), basicCompareOpenGLPoint);
 		arrayOfPoints.push_back(points);
 	}
 
